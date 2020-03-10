f2a9b334afca1710a8d9be632f7813446b2af59d
Alexey <alexeyip68@gmail.com>
2020-03-10 07:37:15 +0200

    Fourth commit

--- jetro/js/main.js
+++ jetro/js/main.js
@@ -2,9 +2,14 @@ $(function () {
   $('.slider__inner').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
     fade: true,
-    asNavFor: '.slider__nav'
+    asNavFor: '.slider__nav',
+    appendArrows: $('.prev .next'),
+    prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i> Туда</button>',
+    nextArrow: '<button id="next" type="button" class="btn btn-juliet">Сюда <i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+    // prevArrow: '<div class="prev"></div>',
+    // nextArrow: '<div class="next"></div>'
   });
 
   $('.slider__nav').slick({
